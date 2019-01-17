884c87a661a87a93d1d2aa05e601c36fb4176b77
Junyu Chen <JunyuChen-0115@users.noreply.github.com>
2019-01-16 22:45:45 -0700

    Initial commit
    Add kafka-client dependency

---
+++ src/main/java/VehiclePositionProducer.java
@@ -0,0 +1,7 @@
+import org.jychen.vehicle.position.api.integration.dto.VehiclePositionDTO;
+
+public class VehiclePositionProducer {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
