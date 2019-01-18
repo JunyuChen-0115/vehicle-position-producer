2b644735c3b0c9819c24a98114388bb4e7ff733a
Junyu Chen <JunyuChen-0115@users.noreply.github.com>
2019-01-17 23:03:01 -0700

    Set up spring starter project

---
+++ src/main/java/org/jychen/vehiclepositionproducer/VehiclePositionProducerApplication.java
@@ -0,0 +1,14 @@
+package org.jychen.vehiclepositionproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclePositionProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclePositionProducerApplication.class, args);
+	}
+
+}
+
